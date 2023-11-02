88555d0b61c2783b8fbcc8b715903da5b2f7edd5
rgovat <rkg0094@tamu.edu>
2023-11-02 17:35:41 -0500

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DTC_Dental.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
