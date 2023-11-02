06d8582ee9e6e148e58cff642ba0e023fce8769d
rgovat <rkg0094@tamu.edu>
2023-11-02 18:02:40 -0500

    FINAL add of Patient entity

--- Models/Patient.cs
+++ Models/Patient.cs
@@ -2,5 +2,24 @@
 {
     public class Patient
     {
+
+        public int PatientId { get; set; }
+        public string PatientFirstName { get; set;}
+        public string PatientLastName { get; set;}
+        public string PatientStreetAddress {  get; set;}
+        public string PatientCity { get; set;}
+        public string PatientState { get; set; }
+        public string PatientZip { get; set;}
+        public string PatientPhone { get; set;}
+        public string PatientEmail { get; set;}
+        public string PatientSSN { get; set;}
+        public DateTime PatientDOB { get; set;}
+        public bool PatientMinor { get; set; }
+        public int PatientHOHlD { get; set;}
+
+
+
+
+
     }
 }
