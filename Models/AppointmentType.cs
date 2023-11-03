0df1d57d2e52bc21e5991e7411b584dcec6a3c28
rgovat <rkg0094@tamu.edu>
2023-11-03 00:06:56 -0500

    added AppointmentType entity + fields

---
+++ Models/AppointmentType.cs
@@ -0,0 +1,10 @@
+﻿namespace DTC_Dental.Models
+{
+    public class AppointmentType
+    {
+        public int TyoeID{  get; set; }
+        public string AppointmentName {  get; set; }
+        public string Description {  get; set; }
+        public int Duration {  get; set; }
+    }
+}
