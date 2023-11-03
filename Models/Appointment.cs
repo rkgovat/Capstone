591f3921a698b832d7d7c86ffb8e081a5d9cb67f
rgovat <rkg0094@tamu.edu>
2023-11-03 00:12:01 -0500

    created Appointment entity + fields

---
+++ Models/Appointment.cs
@@ -0,0 +1,14 @@
+﻿namespace DTC_Dental.Models
+{
+    public class Appointment
+    {
+        public int AppointmentID { get; set; }
+        public int DentistID {  get; set; }
+        public int PatientID {  get; set; }
+        public int TypeID {  get; set; }
+        public DateTime AppointmentDate { get; set; }
+        public TimeOnly StartTime {  get; set; }
+
+
+    }
+}
