bce082086c0b1458c02ceb728a7d0ae54d22c4ec
rgovat <rkg0094@tamu.edu>
2023-11-02 23:55:45 -0500

    created service entity and added fields

---
+++ Models/Service.cs
@@ -0,0 +1,9 @@
+﻿namespace DTC_Dental.Models
+{
+    public class Service
+    {
+        public int ServiceID {  get; set; }
+        public string ServiceDescription {  get; set; }
+        public float ServiceCost {  get; set; }
+    }
+}
